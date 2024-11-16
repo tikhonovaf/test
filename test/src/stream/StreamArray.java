8ab589b92c13e4b92afa0cea40a4f301172a6e25
atikhonov <tikhonovaf@gmail.com>
2024-11-16 22:25:16 +0300

    Added samples

---
+++ test/src/stream/StreamArray.java
@@ -0,0 +1,40 @@
+package stream;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.LongStream;
+import java.util.stream.Stream;
+
+public class StreamArray {
+    public static void main(String[] args) {
+//        IntStream intStream = IntStream.range(1, 3);
+//        LongStream longStream = LongStream.rangeClosed(1, 3);
+//
+//        int reducedParams = Stream.of(1, 2, 3)
+//                .reduce(10, (a, b) -> a + b, (a, b) -> {
+//                    System.out.println("combiner was called");
+//                    return a + b;
+//                });
+        int [] ia = new int [] {1,2,2,4,5};
+////        Arrays.stream(new int[]{1,2,2,4,5})
+//        Arrays.stream(ia)
+//                .forEach(x -> {
+//                    System.out.println("forEach: " + x);
+//                })
+//        ;
+//
+//        var res = Arrays.stream(ia)
+//                .toArray();
+//        System.out.println("toArray: " + ia);
+
+        var sum = Arrays.stream(ia)
+                .average();
+        System.out.println("average: " + sum);
+
+
+
+    }
+}
