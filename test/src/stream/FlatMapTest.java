9b167fb606e9a19c90e49a5e299cec4402beba0c
atikhonov <tikhonovaf@gmail.com>
2024-11-26 16:09:03 +0300

    Added samples

--- test/src/stream/FlatMapTest.java
+++ test/src/stream/FlatMapTest.java
@@ -1,4 +1,29 @@
 package stream;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 public class FlatMapTest {
+
+    public static void main(String[] args) {
+
+        Map<Integer, List<Long>> inputMap = new HashMap<>();
+        inputMap.put(1, Arrays.asList(0L, 1L, 2L));
+        inputMap.put(2,Arrays.asList(3L,4L));
+
+        Map<Long, Integer> outputMap = inputMap.entrySet().stream()
+                .flatMap(entry -> {
+                    System.out.println("entry  key " + entry.getKey());
+                    var key = entry.getKey();
+                    var value = entry.getValue();
+                    return value.stream().map(num -> Map.entry(num,key));
+                })
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+
+        System.out.println(outputMap);
+    }
+
 }
--- test/src/temp/testing/SortIntArray.java
+++ test/src/temp/testing/SortIntArray.java
@@ -9,13 +9,18 @@ import java.util.stream.Collectors;
 public class SortIntArray {
 
     public static void main(String[] args) {
-        int[] first = new int[]{3, 44, 6, 14, 23, 23, 254, 29, 30, 33, 36, 40, 766};
-        int[] second = new int[]{2, 36, 6, 766, 15, 21, 26, 33, 39};
+//        int[] first = new int[]{3, 44, 6, 14, 23, 23, 254, 29, 30, 33, 36, 40, 766};
+        int[] first = {3, 44, 6, 14, 23, 23, 254, 29, 30, 33, 36, 40, 766};
+        int[] second = {2, 36, 6, 766, 15, 21, 26, 33, 39};
 
-        Set<Integer> l1 = Arrays.stream(first).boxed().collect(Collectors.toSet());
+        Set<Integer> l1 = Arrays.stream(first)
+                .boxed()
+                .collect(Collectors.toSet());
         OptionalInt maxValue =
                 Arrays.stream(second)
-                        .filter(v-> (l1.contains(v)))
+                        .filter(v-> {System.out.println(v);
+                            return l1.contains(v);
+                            })
                         .max();
 
         System.out.println(maxValue);
