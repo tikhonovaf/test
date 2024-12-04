44bebdc2fbf01982c3c64b0f082215e6ead19cce
atikhonov <tikhonovaf@gmail.com>
2024-12-05 00:02:19 +0300

    Added samples

--- test/src/stream/StreamSeq.java
+++ test/src/stream/StreamSeq.java
@@ -1,10 +1,24 @@
 package stream;
 
+import lombok.Data;
+
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 public class StreamSeq {
 
     public static void main(String[] args) {
+
+        List<Room> rooms = new ArrayList<>();
+        Integer minAge = 12;
+        var room = rooms.stream()
+                .filter(r -> r.getPersons().stream()
+                        .anyMatch(person -> person.getAge() < minAge)
+                )
+                .map(Room::getNumber)
+                .findFirst();
+
         final List<String> list = List.of("dog", "cat", "hamster");
         list.stream()
                 .filter(s -> {
@@ -20,5 +34,17 @@ public class StreamSeq {
                 .forEach(x -> {
                     System.out.println("forEach: " + x);
                 });
+    }
+
+    @Data
+    class Person {
+        private String name;
+        private int age;
+    }
+
+    @Data
+    class Room {
+        private int number;
+        private List<Person> persons;
     }
 }
