b3c62ed6a41508ca88fd482e031268664a7e0d74
atikhonov <tikhonovaf@gmail.com>
2024-12-05 16:52:24 +0300

    Added samples

--- test/src/stream/StreamSeq.java
+++ test/src/stream/StreamSeq.java
@@ -11,13 +11,27 @@ public class StreamSeq {
     public static void main(String[] args) {
 
         List<Room> rooms = new ArrayList<>();
-        Integer minAge = 12;
-        var room = rooms.stream()
+        List<Person> personList1 = new ArrayList<>();
+        personList1.add(new Person("Сергей", 12));
+        personList1.add(new Person("Иван", 14));
+        personList1.add(new Person("Михаил", 15));
+        rooms.add(new Room(2, personList1));
+
+        List<Person> personList2 = new ArrayList<>();
+        personList1.add(new Person("Сергей", 22));
+        personList1.add(new Person("Иван", 24));
+        personList1.add(new Person("Михаил", 25));
+        rooms.add(new Room(2, personList2));
+
+        Integer minAge = 14;
+        var roomNum = rooms.stream()
                 .filter(r -> r.getPersons().stream()
                         .anyMatch(person -> person.getAge() < minAge)
                 )
                 .map(Room::getNumber)
                 .findFirst();
+
+        System.out.println("map: " + roomNum);
 
         final List<String> list = List.of("dog", "cat", "hamster");
         list.stream()
@@ -37,14 +51,24 @@ public class StreamSeq {
     }
 
     @Data
-    class Person {
+    static class Person {
         private String name;
         private int age;
+
+        public Person(String name, int age) {
+            this.name = name;
+            this.age = age;
+        }
     }
 
     @Data
-    class Room {
+    static class Room {
         private int number;
         private List<Person> persons;
+
+        public Room(int number, List<Person> persons) {
+            this.persons = persons;
+            this.number = number;
+        }
     }
 }
